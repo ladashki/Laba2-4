d980e6ba8ec37420fa2576e95a57635ecbb9bcfc
ladashki <61075875+ladashki@users.noreply.github.com>
2020-05-06 16:18:51 +0300

    Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -17,9 +17,14 @@ module.exports = {
     module: {
         rules: [
           {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: ['babel-loader']
+             test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader" },
+        },
+        {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"]
         }
       ]
     }
