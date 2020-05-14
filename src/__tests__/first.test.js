9f7251f2ec41c113d104e86eb9d26516dc98fa05
ladashki <61075875+ladashki@users.noreply.github.com>
2020-05-14 17:42:57 +0300

    Import fix

--- src/__tests__/first.test.js
+++ src/__tests__/first.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import Info from "./components/info";
+import QuoteShow from "./components/quote";
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
